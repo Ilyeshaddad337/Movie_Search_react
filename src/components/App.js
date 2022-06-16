5d184591d3725afe879912953308338a9bd86076
Ilyeshaddad337 <i.haddad@esi-sba.dz>
2022-06-16 12:27:59 +0100

    iniialising

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import '../App.css';
+import Header from './Header.js'
+function App() {
+  return (
+   <Header className ='Header' text='Movie Search App'></Header>
+  );
+}
+
+export default App;
