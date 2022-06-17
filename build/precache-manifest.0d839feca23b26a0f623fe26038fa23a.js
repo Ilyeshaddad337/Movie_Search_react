5342ad8fa8cb1eea7131882eaf744d27620cbb21
Ilyeshaddad337 <i.haddad@esi-sba.dz>
2022-06-17 18:59:14 +0100

    added build folder

---
+++ build/precache-manifest.0d839feca23b26a0f623fe26038fa23a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "249c112bafceb6983427",
+    "url": "/static/css/main.a9c36071.chunk.css"
+  },
+  {
+    "revision": "249c112bafceb6983427",
+    "url": "/static/js/main.249c112b.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "c17fbe7d49a1938163cfb3cd32cd1714",
+    "url": "/index.html"
+  }
+];
