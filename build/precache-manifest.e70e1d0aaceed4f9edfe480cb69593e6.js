a6fd0c51524ffe1c798f92914c11065d625e3c50
Ilyeshaddad337 <i.haddad@esi-sba.dz>
2022-06-18 11:57:46 +0100

    finally undersatnded the error

---
+++ build/precache-manifest.e70e1d0aaceed4f9edfe480cb69593e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3602ef43f35166429175",
+    "url": "/static/css/main.a9c36071.chunk.css"
+  },
+  {
+    "revision": "3602ef43f35166429175",
+    "url": "/static/js/main.3602ef43.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "e0e97c27e949d2b0c8be0601de088b76",
+    "url": "/index.html"
+  }
+];
